438ec31cfd3a1a29efeb0464dd0da78463b5210b
Jung taeWoong <60641307+jtwjs@users.noreply.github.com>
2020-12-30 16:32:28 +0900

    assets: bug image

---
+++ Game/canvas_bug.js
@@ -0,0 +1,24 @@
+export default class Bug {
+    constructor(index, x, y, speed) {
+        this.index = index;
+        this.x = x;
+        this.y = y;
+        this.speed = speed;
+        this.width = 160;
+        this.height = 160;
+        this.img = new Image();
+        this.img.src = './assets/img/bug.png';
+        this.img2 = new Image();
+        this.img2.src = './assets/img/bug2.png';
+        this.motion = false;
+        this.ctx = document.querySelector('.canvas-field').getContext('2d');
+    }
+
+    draw() {
+        if(this.motion) {
+            this.ctx.drawImage(this.img2, this.x, this.y, this.width, this.height);
+        } else{
+            this.ctx.drawImage(this.img, this.x, this.y, this.width, this.height);
+        }
+        }
+    }
